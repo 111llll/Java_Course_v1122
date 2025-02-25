aee517d96818bb54ab460a6d8df5fad0281501c4
Liang <1762zx@gmail.com>
2025-02-25 16:40:44 +0800

    Create Lab05.java

---
+++ FCPS_eUnits/Unit1/src/Lab05.java
@@ -0,0 +1,23 @@
+import edu.fcps.karel2.Display;
+
+public class Lab05 {
+    public static void runTheRace(Racer RandomGuy)
+    {
+        RandomGuy.shuttle(3, 7);
+        RandomGuy.shuttle(5, 5);
+        RandomGuy.shuttle(7, 3);
+        RandomGuy.move();
+    }
+    public static void main(String[] args) {
+        Display.openWorld("maps/shuttle.map");
+        Display.setSize(10, 10);
+
+        Racer TopGuy = new Racer(7);
+        Racer MiddleGuy = new Racer(4);
+        Racer BottomGuy = new Racer(1);
+
+        runTheRace(TopGuy);
+        runTheRace(MiddleGuy);
+        runTheRace(BottomGuy);
+    }
+}
