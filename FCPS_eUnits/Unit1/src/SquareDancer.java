a1f9b5d39958783eac9452a60d10902ea43d8d83
Liang <1762zx@gmail.com>
2025-02-26 19:53:50 +0800

    Inherit from Dancer

---
+++ FCPS_eUnits/Unit1/src/SquareDancer.java
@@ -0,0 +1,14 @@
+public class SquareDancer extends Dancer {
+    public SquareDancer(int x,int y,int dir){
+        super(x,y,dir,0);
+    }
+    public void danceStep(){
+        for(int i = 1;i <= 4;i++){
+            move();
+            move();
+            move();
+            move();
+            turnRight();
+        }
+    }
+}
