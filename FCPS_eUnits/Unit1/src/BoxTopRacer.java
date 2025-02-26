109069ab1a90917d571d58f0bac67ac565052eba
Liang <1762zx@gmail.com>
2025-02-26 19:54:37 +0800

    Inherit from Racer

---
+++ FCPS_eUnits/Unit1/src/BoxTopRacer.java
@@ -0,0 +1,22 @@
+public class BoxTopRacer extends Racer {
+    public BoxTopRacer(int y){
+        super(y);
+    }
+    public void jumpRight()
+    {
+        turnLeft();
+        while(!rightIsClear()){
+            move();
+        }
+        turnRight();
+        move();
+        while(!rightIsClear()){
+            move();
+        }
+        turnRight();
+        while(frontIsClear()){
+            move();
+        }
+        turnLeft();
+    }
+}
