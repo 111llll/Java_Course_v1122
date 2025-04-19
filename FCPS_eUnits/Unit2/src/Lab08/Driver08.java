7c015886df3bb4fb1c4da1f00678af65bb3c1461
Liang <1762zx@gmail.com>
2025-04-19 23:10:31 +0800

    Update Driver08.java

--- FCPS_eUnits/Unit2/src/Lab08/Driver08.java
+++ FCPS_eUnits/Unit2/src/Lab08/Driver08.java
@@ -1,17 +1,38 @@
 	//Name______________________________ Date_____________
+package Lab08;
    import edu.fcps.Turtle;
    import java.awt.Color;
-   import javax.swing.*;
+import java.util.concurrent.Flow;
+
+import javax.swing.*;
+
+import Lab06.TurtlePanel;
    public class Driver08
    {
       public static void main(String[] args)
       {
-      
-      	/************************/
-      	/*                      */
-      	/* Your code goes here. */
-      	/*                      */
-      	/************************/
+         JFrame frame = new JFrame("Flower Turtles");
+         frame.setSize(700,700);
+         frame.setLocation(200,100);
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setContentPane(new TurtlePanel());
+         frame.setVisible(true);
+         Turtle.clear(Color.white);
+
+         FlowerTurtle flowerTurtle1 = new FlowerTurtle(100,45,Color.red);
+         FlowerTurtle flowerTurtle2 = new FlowerTurtle(250,45,Color.yellow);
+         FlowerTurtle flowerTurtle3 = new FlowerTurtle(400,45,Color.blue);
+         FlowerTurtle flowerTurtle4 = new FlowerTurtle(550,45,Color.black);
+
+         Thread t1 = new Thread( flowerTurtle1 );
+         Thread t2 = new Thread( flowerTurtle2 );
+         Thread t3 = new Thread( flowerTurtle3 );
+         Thread t4 = new Thread( flowerTurtle4 );
+
+         t1.start();
+         t2.start();
+         t3.start();
+         t4.start();
       
       }
    }
